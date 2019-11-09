6cf37f0d2ae9238469b3961a31ce5cbb7927da19
camron-rollins <camron.rollins@hgs.hiddengeniusproject.org>
2019-11-09 13:18:12 -0800

    made js_project and true or false game

---
+++ my_life_journey.js
@@ -0,0 +1,31 @@
+function myLifeJourney() {
+  //Array Of Passions
+  var myPassions = ["Drawing", "Gaming", "Sports", "School" , "Coding" , "Anime"];
+  //Array Of Career Options
+  var myCarreerOpts = ["Video game concept artist", "Proffesional Gamer", "Manga artist/creator", "Sports" , "Lawyer" , "Beat producer"];
+
+  //It Is True That I am Alive
+  var alive = true;
+
+  while (alive) {
+graduateWith3_8orHigher();
+passFinalsForAllClassess();
+completeHighSchoolOnTime();
+getInternship();
+goToCollegeAtUCLA();
+majorIn
+makeAMangaAndAnime();
+workAtA
+  }
+}
+
+
+function liveCurrentPathway(goals, careers){
+  for(var i =0; i< goals.length; i++){
+    for(var j =0; j< careers.length; j++){
+      if(goals[i]==careers[j]){
+        doStepsToBuildCareer(careers[j]);
+      }
+    }
+  }
+}
